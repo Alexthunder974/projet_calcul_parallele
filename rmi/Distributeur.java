b6e4de9dfb3499ee899c3001010cbb380478df35
tricki-lucky7 <remi.perrier1@etu.univ-lorraine.fr>
2023-06-09 09:15:06 +0200

    gestion des erreurs distributeur

--- rmi/Distributeur.java
+++ rmi/Distributeur.java
@@ -1,27 +1,42 @@
 package rmi;
 
 import java.rmi.RemoteException;
+import java.rmi.server.RemoteServer;
+import java.rmi.server.ServerNotActiveException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Distributeur implements ServiceDistributeur {
 
+    private static final boolean DEBUG = false;
     private List<ServiceNoeud> noeuds = new ArrayList<>();
     private int noeudCourant = 0;
 
     @Override
-    public void enregistrerNoeud(ServiceNoeud noeud) throws RemoteException {
-        noeuds.add(noeud);
+    public void enregistrerNoeud(ServiceNoeud noeud) {
+        String host = "";
+        try{
+            host = RemoteServer.getClientHost();
+        }catch(ServerNotActiveException e) {
+            System.out.println("erreur : machine inconnue");
+            System.exit(1);
+        }
+
+        if (DEBUG) {
+            System.out.println("Connexion de " + host);
+        }
+
+        this.noeuds.add(noeud);
     }
 
     @Override
-    public void retirerNoeud(ServiceNoeud noeud) throws RemoteException {
-        noeuds.remove(noeud);
+    public void retirerNoeud(ServiceNoeud noeud) {
+        this.noeuds.remove(noeud);
     }
 
     @Override
     public ServiceNoeud getNoeud() throws RemoteException {
-        ServiceNoeud n = noeuds.get(noeudCourant);
+        ServiceNoeud n = this.noeuds.get(noeudCourant);
         noeudCourant = (noeudCourant == (noeuds.size() - 1)) ? 0 : noeudCourant+1;
         return n;
     }
