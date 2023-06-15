96fca1a92c5bed247dc920a422a5df89dc5604c0
Alexandre Noel <noel295u@iutnc-127-01.ad.univ-lorraine.fr>
2023-06-15 17:08:55 +0200

    modif script bash, fonctionne pour la salle 125

--- script/main_script.sh
+++ script/main_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chemin="cd ~/nas/Documment/2A/S4/R4.01/Projet/trace_de_rayon"
-exec="java Noeud ip port"
+chemin="cd ~/Documents/projet_calcul_parallele/out/production/projet_calcul_parallele"
+exec="java rmi.LancerNoeud 100.64.80.209 1099"
 for i in {224..245}
 do
   ssh -o ConnectTimeout=1 -o StrictHostKeyChecking=accept-new 100.64.80.$i "${chemin} && ${exec} &" &
